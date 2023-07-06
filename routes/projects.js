48587a7e2291c904a1644269464d79627910a566
Intuitive Harmony <j.horst77@gmail.com>
2023-07-06 10:12:19 -0600

    projects page start

--- routes/projects.js
+++ routes/projects.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  res.render("projects", { title: "Projects" });
 });
 
 module.exports = router;
