29cbac6a98910bc1c968c0aa2d8ebeefd346ced2
Intuitive Harmony <j.horst77@gmail.com>
2023-07-06 14:16:07 -0600

    start on admin

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const favicon = require("serve-favicon");
 
 const indexRouter = require("./routes/index");
 const projectsRouter = require("./routes/projects");
+const adminRouter = require("./routes/admin");
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/projects", projectsRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
