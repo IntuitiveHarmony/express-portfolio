67c4d4ec3e3053d5670fc1ff1eeb0e5f000e8763
Intuitive Harmony <j.horst77@gmail.com>
2023-07-25 19:22:43 -0600

    project cards, borders, start on nav

--- models/project.js
+++ models/project.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  img: { type: String },
-  liveLink: { type: String },
-  description: { type: String },
-  githubLink: { type: String },
-  display: { type: Boolean },
-});
+const projectSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    img: { type: String },
+    liveLink: { type: String },
+    description: { type: String },
+    githubLink: { type: String },
+    display: { type: Boolean },
+  },
+  { timestamps: true }
+);
 
 const Project = mongoose.model("Project", projectSchema);
 
