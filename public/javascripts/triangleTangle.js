650c482d69c175b38d7167c2d10581534e35d8c6
Intuitive Harmony <j.horst77@gmail.com>
2023-07-26 20:33:21 -0600

    footer - droppin triangles

---
+++ public/javascripts/triangleTangle.js
@@ -0,0 +1,32 @@
+let numTriangles;
+
+const dropTriangles = () => {
+  const container = $(".triangle-container");
+  container.children(".triangle").remove();
+
+  for (let i = 0; i < numTriangles; i++) {
+    container.append(`<div class="triangle" id=${i + 1}>`);
+  }
+};
+
+const checkScreenWidth = () => {
+  let screenWidth = $(window).width();
+  console.log("Screen Width: " + screenWidth + "px");
+
+  if (screenWidth <= 768) {
+    numTriangles = 7;
+  } else if (screenWidth <= 1024) {
+    numTriangles = 13;
+  } else {
+    numTriangles = 23;
+  }
+};
+
+$(() => {
+  checkScreenWidth();
+  dropTriangles();
+  $(window).resize(function () {
+    checkScreenWidth();
+    dropTriangles();
+  });
+});
